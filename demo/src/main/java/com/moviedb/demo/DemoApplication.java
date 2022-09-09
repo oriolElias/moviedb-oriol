f78cf6f0bbb1548e71aeab10945d9f26eeb1ebd3
oriolElias <Oriol.Elias@gft.com>
2022-09-09 09:40:04 +0000

    Change to gemfire

--- demo/src/main/java/com/moviedb/demo/DemoApplication.java
+++ demo/src/main/java/com/moviedb/demo/DemoApplication.java
@@ -1,16 +1,23 @@
 package com.moviedb.demo;
 
+import org.apache.geode.cache.client.ClientRegionShortcut;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cache.annotation.EnableCaching;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.data.gemfire.cache.config.EnableGemfireCaching;
+import org.springframework.data.gemfire.config.annotation.ClientCacheApplication;
+import org.springframework.data.gemfire.config.annotation.EnableCachingDefinedRegions;
 
 import java.util.Arrays;
 
 
 @SpringBootApplication
-@EnableCaching
-@Configuration
+//@EnableCaching
+//@Configuration
+@ClientCacheApplication(name = "CachingGemFireApplication")
+@EnableCachingDefinedRegions(clientRegionShortcut = ClientRegionShortcut.LOCAL)
+@EnableGemfireCaching
 public class DemoApplication {
 
 	public static void main(String[] args) {
