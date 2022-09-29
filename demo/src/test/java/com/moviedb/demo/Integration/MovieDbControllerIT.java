3d1a800b59077b50f8eae4bb5347b58b08ecb8e5
oriolElias <Oriol.Elias@gft.com>
2022-09-29 10:47:01 +0000

    First integration test

---
+++ demo/src/test/java/com/moviedb/demo/Integration/MovieDbControllerIT.java
@@ -0,0 +1,70 @@
+package com.moviedb.demo.Integration;
+
+import com.moviedb.demo.Controller.MovieDbController;
+import org.apache.geode.internal.Assert;
+import org.h2.jdbcx.JdbcDataSource;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.mock.web.MockServletContext;
+import org.springframework.security.test.context.support.WithMockUser;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
+import org.springframework.test.context.web.WebAppConfiguration;
+
+
+
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.MvcResult;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
+import org.springframework.web.context.WebApplicationContext;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+
+import javax.servlet.ServletContext;
+
+import static org.junit.jupiter.api.Assertions.*;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+
+@ExtendWith(SpringExtension.class)
+@ContextConfiguration(classes = { JdbcDataSource.class })
+@WebAppConfiguration(value = "src/main/java/com/moviedb/demo/DemoApplication.java")
+@AutoConfigureMockMvc(addFilters = false)
+@EnableWebMvc
+public class MovieDbControllerIT {
+
+    @Autowired
+    private WebApplicationContext webApplicationContext;
+    @Autowired(required = true)
+    private MockMvc mockMvc;
+
+    @MockBean
+    MovieDbController movieDbController;
+
+    @BeforeEach
+    public void setup() throws Exception {
+        this.mockMvc = MockMvcBuilders.webAppContextSetup(this.webApplicationContext).build();
+    }
+
+    @Test
+    public void givenWac_whenServletContext_thenItProvidesGreetController() {
+        ServletContext servletContext = webApplicationContext.getServletContext();
+
+        assertNotNull(servletContext);
+        assertTrue(servletContext instanceof MockServletContext);
+        assertNotNull(webApplicationContext.getBean(MovieDbController.class));
+    }
+
+    @Test
+    void GIVEN_apiConfiguration_WHEN_MockMvc_THEN_VerifyResponse() throws Exception {
+        MvcResult mvcResult = mockMvc.perform(get("/api/configuration")).andDo(print()).andExpect(status().isOk()).andReturn();
+        assertEquals("application/json",mvcResult.getResponse().getContentType());
+    }
+
+}
